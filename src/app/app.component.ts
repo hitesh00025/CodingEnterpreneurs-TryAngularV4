07e0b28ca37c6d8858ebedc9928b0dd87dd61e62
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-10-18 15:28:07 -0500

    App component and component templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'srvup-root',
   templateUrl: './app.component.html',
+  // template: `<h1>{{ title }}</h1><p>This is my first app using Angular 4</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '#srvup';
 }
