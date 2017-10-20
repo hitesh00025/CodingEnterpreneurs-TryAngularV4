6959b4eb06e5a2fd4f961fc5bf2826c342fc5bbe
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-10-20 12:47:56 -0500

    Creating components with `ng g component`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoListComponent,
+    VideoDetailComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [VideoDetailComponent]
 })
 export class AppModule { }
